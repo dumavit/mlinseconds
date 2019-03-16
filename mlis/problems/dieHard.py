ef8c081bd7d1a9c42f9559216b2c1f726f0de6fe
dumavit <duma1492@gmail.com>
2019-03-16 15:02:52 +0200

    Solve dieHard in ~ 15 steps

--- mlis/problems/dieHard.py
+++ mlis/problems/dieHard.py
@@ -10,78 +10,158 @@
 # Inputs:
 # x0, x1, x2, x3, x4, x5, x6, x7
 # Easy function:
-# x0^x1
 # Hard function:
 # x2^x3^x4^x5^x6^x7
-import time
+# x0^x1
 import random
 import torch
-import torchvision
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
+import numpy as np
 from ..utils import solutionmanager as sm
+from ..utils.gridsearch import GridSearch
 
-class SolutionModel(nn.Module):
-    def __init__(self, input_size, output_size):
-        super(SolutionModel, self).__init__()
-        self.input_size = input_size
-        sm.SolutionManager.print_hint("Hint[1]: NN usually learn easiest function, you need to learn hard one")
-        self.hidden_size = 10
-        self.linear1 = nn.Linear(input_size, self.hidden_size)
-        self.linear2 = nn.Linear(self.hidden_size, output_size)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+class SimpleModel(nn.Module):
+    def __init__(self):
+        super(SimpleModel, self).__init__()
+
+        self.fc1 = nn.Linear(8, 8)
+        self.fc2 = nn.Linear(8, 1)
+
+        self.batch_norm = nn.BatchNorm1d(8, affine=False, track_running_stats=False)
 
     def forward(self, x):
-        x = self.linear1(x)
-        x = F.sigmoid(x)
-        x = self.linear2(x)
-        x = F.sigmoid(x)
+        x = self.fc1(x)
+        x = self.batch_norm(x)
+        x = torch.relu(x)
+
+        x = self.fc2(x)
+        x = torch.sigmoid(x)
+
         return x
+
+
+class SolutionModel(nn.Module):
+    def __init__(self, input_size, output_size, params):
+        super(SolutionModel, self).__init__()
+        self.input_size = input_size
+        self.params = params
+
+        sizes = [input_size] + params.hidden_sizes + [output_size]
+
+        self.layers = nn.ModuleList(
+            nn.Linear(sizes[idx], sizes[idx + 1]) for idx in range(len(sizes) - 1)
+        ).to(device)
+
+        self.batch_norms = nn.ModuleList(
+            nn.BatchNorm1d(size, affine=False, track_running_stats=False) for size in sizes[1:]
+        )
+
+    def forward(self, x):
+        for idx, layer in enumerate(self.layers):
+            x = layer(x)
+
+            if idx != len(self.layers) - 1:
+                x = self.batch_norms[idx](x)
+
+            x = self.params.activations[idx](x)
+        return x
+
 
 class Solution():
     def __init__(self):
-        self = self
+        # NOTE: Network params
+        self.lr = 0.00723571
+
+        self.hidden_sizes = [24, 24, 24]
+        self.momentum = 0.7
+
+        self.activations = [
+            nn.ReLU6(),
+            nn.ReLU6(),
+            nn.ReLU6(),
+            nn.Sigmoid()
+        ]
+
+        # NOTE: Grids
+        self.lr_grid = list(np.linspace(0.0001, 0.1, 15)) + list(np.linspace(0.2, 1.5, 10))
+
+        self.hidden_sizes_grid = [
+            [i, i, i]
+            for i in [24]
+        ]
+
+        # self.momentum_grid = [0, 0.11, 0.357, 0.72]
+
+        self.grid_search = GridSearch(self)
+        self.grid_search.set_enabled(False)
 
     def create_model(self, input_size, output_size):
-        return SolutionModel(input_size, output_size)
+        return SolutionModel(input_size, output_size, self)
 
     # Return number of steps used
-    def train_model(self, model, train_data, train_target, context):
+    def train_model(self, model, train_data, target, context):
         step = 0
         # Put model in train mode
+        model.to(device)
         model.train()
+
+        simple_model = SimpleModel()
+        simple_model.to(device)
+        simple_optimizer = optim.RMSprop(simple_model.parameters(), lr=0.01, momentum=0.7)
+
+        simple_model_results = [0] * 8
+
+        # Train simple model to learn easy function inputs
+        for i in range(10):
+            for col_index in range(8):
+                data = train_data.clone()
+                # Inverse one input (0->1, 1->0)
+                data[:, col_index] = torch.abs(data[:, col_index] - 1)
+
+                simple_optimizer.zero_grad()
+                output = simple_model(data)
+                predict = output.round()
+                correct = predict.eq(target.view_as(predict)).long().sum().item()
+
+                loss = ((output - target) ** 2).sum()
+                loss.backward()
+                simple_optimizer.step()
+                simple_model_results[col_index] = correct
+
+        # When easy input is inverted - model gets the worst results
+        easy_input_1, easy_input_2 = np.argpartition(simple_model_results, 2)[:2]
+        hard_data = train_data.clone()
+
+        # Delete easy inputs to learn hard function
+        hard_data[:, easy_input_1] = 0
+        hard_data[:, easy_input_2] = 0
+
+        optimizer = optim.RMSprop(model.parameters(), lr=self.lr, momentum=self.momentum)
+        total = target.view(-1).size(0)
+
         while True:
             time_left = context.get_timer().get_time_left()
-            # No more time left, stop training
             if time_left < 0.1:
                 break
-            optimizer = optim.SGD(model.parameters(), lr=0.1)
-            data = train_data
-            target = train_target
-            # model.parameters()...gradient set to zero
+
             optimizer.zero_grad()
-            # evaluate model => model.forward(data)
-            output = model(data)
-            # if x < 0.5 predict 0 else predict 1
+            output = model(hard_data)
             predict = output.round()
-            # Number of correct predictions
             correct = predict.eq(target.view_as(predict)).long().sum().item()
-            # Total number of needed predictions
-            total = target.view(-1).size(0)
-            # calculate loss
-            loss = ((output-target)**2).sum()
-            # calculate deriviative of model.forward() and put it in model.parameters()...gradient
+            loss = ((output - target) ** 2).sum()
+
+            if correct == total:
+                break
+
             loss.backward()
-            # print progress of the learning
-            self.print_stats(step, loss, correct, total)
-            # update model: model.parameters() -= lr * gradient
             optimizer.step()
             step += 1
         return step
 
-    def print_stats(self, step, loss, correct, total):
-        if step % 1000 == 0:
-            print("Step = {} Prediction = {}/{} Error = {}".format(step, correct, total, loss.item()))
 
 ###
 ###
@@ -93,6 +173,7 @@ class Limits:
         self.time_limit = 2.0
         self.size_limit = 1000000
         self.test_limit = 0.75
+
 
 class DataProvider:
     def __init__(self):
@@ -125,8 +206,8 @@ class DataProvider:
     def create_data(self, seed, easy_table, hard_table, easy_input_size, hard_input_size, easy_correct):
         input_size = easy_input_size + hard_input_size
         data_size = 1 << input_size
-        data = torch.ByteTensor(data_size, input_size)
-        target = torch.ByteTensor(data_size, 1)
+        data = torch.ByteTensor(data_size, input_size).to(device)
+        target = torch.ByteTensor(data_size, 1).to(device)
         count = 0
         for ind in range(data_size):
             self.int_to_tensor(ind, data[count])
